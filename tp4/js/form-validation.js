ddf62d21160cd1ffdf4f6010cd40be1502ce95e5
BELLAHCENE28 <celinabellahcene2811@gmail.com>
2024-10-21 19:18:16 +0200

    TP4

--- tp4/js/form-validation.js
+++ tp4/js/form-validation.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+    displayContactList();
     const form = document.querySelector("#myForm"); // Assurez-vous que le bon ID de formulaire est utilisé
     const contactsTable = document.querySelector("#contactsTable tbody");
 
@@ -6,6 +7,7 @@ window.onload = function () {
         event.preventDefault(); // Empêche le comportement par défaut de soumission
 
         let formValid = true;
+
 
         // Suppression des messages d'erreur précédents
         clearErrorMessages();
@@ -39,12 +41,18 @@ window.onload = function () {
             formValid = false;
         }
 
+
+        const nom = document.getElementById("nom").value;
+        const prenom = document.getElementById("prenom").value;
+        const adresse = document.getElementById("adressePostale").value;
+
+
         // Affichage du modal d'erreur ou ajout du contact
         if (!formValid) {
             showErrorModal();
         } else {
-            // Si le formulaire est valide, ajout du contact au tableau
-            ajouterContact();
+            contactStore.add(nom, prenom, dateNaissance, adresse, email);
+            displayContactList()
             form.reset(); // Réinitialise le formulaire
         }
     });
@@ -77,6 +85,10 @@ window.onload = function () {
             input.classList.remove("is-invalid");
         });
     }
+    function showImage() {
+        getLocation()
+        document.getElementById('gpsImage').style.display = 'block';
+    }
 
     // Fonction pour afficher le modal d'erreur
     function showErrorModal() {
@@ -85,23 +97,69 @@ window.onload = function () {
     }
 
     // Fonction pour ajouter un contact au tableau
-    function ajouterContact() {
-        const nom = document.getElementById("nom").value;
-        const prenom = document.getElementById("prenom").value;
-        const dateNaissance = document.getElementById("dateNaissance").value;
-        const adresse = document.getElementById("adressePostale").value;
-        const email = document.getElementById("email").value;
 
-        const row = document.createElement("tr");
+    function displayContactList() {
+        const contactListString = localStorage.getItem('contactList'); // ici on va récupérer la liste en forme de chaine de caractère (string)
+        const contactList = contactListString ? JSON.parse(contactListString) : [];
+        document.querySelector("table tbody").innerHTML = "";
 
-        row.innerHTML = `
-            <td>${nom}</td>
-            <td>${prenom}</td>
-            <td>${dateNaissance}</td>
-            <td>${adresse}</td>
-            <td>${email}</td>
-        `;
-
-        contactsTable.appendChild(row);
+        for (const contact of contactList) {
+            document.querySelector("table tbody").innerHTML +=
+                `<tr>
+        <td>${contact.name}</td>
+        <td> ${contact.firstname} </td>
+        <td> ${contact.date} </td>
+        <td> ${contact.adress} </td>
+        <td> ${contact.mail} </td>
+        <tr>`;
+        }
     }
+
+    const btnReset = document.querySelector(".reset");
+
+    btnReset.addEventListener("click", function () {
+        contactStore.reset();
+        displayContactList()
+    });
+
+    // function resetContactList() {
+    //     contactStore.reset();
+    //     displayContactList()
+
+    // }
+    const btnLocaliser = document.querySelector(".mapbtn");
+
+    btnLocaliser.addEventListener("click", function () {
+        getLocation();
+    });
+
+    const fieldsToTrack = ["nom", "prenom", "adressePostale", "email", "dateNaissance"];
+    fieldsToTrack.forEach(function (field) {
+        const inputField = document.getElementById(field);
+        inputField.addEventListener("keyup", function () {
+            calcNbChar(inputField);
+        });
+    });
+
 };
+
+function showMapModal() {
+    const mapModalBody = document.querySelector(".modal-body");
+    const mapImage = `
+    <a href="http://maps.google.com/maps?q=Paris" target="_blank">
+      <img src="https://maps.googleapis.com/maps/api/staticmap?markers=Paris&zoom=14&size=400x300&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg" alt="Carte de Paris" class="img-fluid" />
+    </a>
+  `;
+    mapModalBody.innerHTML = mapImage;
+
+    // Afficher la modale de la carte
+    var mapModal = new bootstrap.Modal(document.getElementById('mapModal'));
+    mapModal.show();
+}
+function calcNbChar(id) {
+    document.querySelector(`#charCount${id}`).textContent = document.querySelector(
+        `#${id}`
+    ).value.length + ' car.';
+    console.log('Nombre de caractères:');
+
+}
